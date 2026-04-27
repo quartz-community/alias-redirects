9336ec67662735119bc0f8d6389b24cf338ba75c
saberzero1 <github@emilebangma.com>
2026-04-27 21:37:14 +0200

    build: bundle dependencies and ship pre-built dist

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,75 @@
 import path from 'path';
 import fs from 'fs/promises';
-import { joinSegments } from '@quartz-community/types';
-import { simplifySlug, isRelativeURL, resolveRelative } from '@quartz-community/utils';
 
 // src/emitter.ts
-var write = async (ctx, slug, ext, content) => {
-  const pathToPage = joinSegments(ctx.argv.output, slug + ext);
+
+// node_modules/@quartz-community/types/dist/index.js
+function joinSegments(...segments) {
+  return segments.filter((segment) => segment.length > 0).join("/").replace(/\/+/g, "/");
+}
+"function" == typeof Promise ? Promise.prototype.then.bind(Promise.resolve()) : setTimeout, Math.random().toString(8);
+
+// node_modules/@quartz-community/utils/dist/index.js
+function isRelativeURL(s2) {
+  const validStart = /^\.{1,2}/.test(s2);
+  const validEnding = !endsWith(s2, "index");
+  return validStart && validEnding && ![".md", ".html"].includes(getFileExtension(s2) ?? "");
+}
+function simplifySlug(fp) {
+  const res = stripSlashes(trimSuffix(fp, "index"), true);
+  return res.length === 0 ? "/" : res;
+}
+function joinSegments2(...args) {
+  if (args.length === 0) {
+    return "";
+  }
+  let joined = args.filter((segment) => segment !== "" && segment !== "/").map((segment) => stripSlashes(segment)).join("/");
+  const first = args[0];
+  const last = args[args.length - 1];
+  if (first?.startsWith("/")) {
+    joined = "/" + joined;
+  }
+  if (last?.endsWith("/")) {
+    joined = joined + "/";
+  }
+  return joined;
+}
+function endsWith(s2, suffix) {
+  return s2 === suffix || s2.endsWith("/" + suffix);
+}
+function trimSuffix(s2, suffix) {
+  if (endsWith(s2, suffix)) {
+    s2 = s2.slice(0, -suffix.length);
+  }
+  return s2;
+}
+function stripSlashes(s2, onlyStripPrefix) {
+  if (s2.startsWith("/")) {
+    s2 = s2.substring(1);
+  }
+  if (!onlyStripPrefix && s2.endsWith("/")) {
+    s2 = s2.slice(0, -1);
+  }
+  return s2;
+}
+function getFileExtension(s2) {
+  return s2.match(/\.[A-Za-z0-9]+$/)?.[0];
+}
+function pathToRoot(slug2) {
+  let rootPath = slug2.split("/").filter((x2) => x2 !== "").slice(0, -1).map((_2) => "..").join("/");
+  if (rootPath.length === 0) {
+    rootPath = ".";
+  }
+  return rootPath;
+}
+function resolveRelative(current, target) {
+  const res = joinSegments2(pathToRoot(current), simplifySlug(target));
+  return res;
+}
+
+// src/emitter.ts
+var write = async (ctx, slug2, ext, content) => {
+  const pathToPage = joinSegments(ctx.argv.output, slug2 + ext);
   const dir = path.dirname(pathToPage);
   await fs.mkdir(dir, { recursive: true });
   await fs.writeFile(pathToPage, content);
